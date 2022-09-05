b1699df73203b7c0e9b25eab95db28e4f1ed6410
Talha <talha2394@gmail.com>
2022-09-05 13:42:11 -0700

    spaces

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -6,6 +6,9 @@ def index(request):
     context_dict = {'text':'hello world','number': 100}
     return render(request, 'basic_app/index.html',context_dict)
 
+
+
+
 def other(request):
     return render(request, 'basic_app/other.html')
 
